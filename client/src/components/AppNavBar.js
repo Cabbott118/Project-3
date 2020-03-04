1687010b33dc39088c8d58c1cdda3c97a206a50e
Caleb Abbott <54942782+Cabbott118@users.noreply.github.com>
2020-03-03 21:01:54 -0500

    Navbar component added (Clean up finished)

---
+++ client/src/components/AppNavBar.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container
+} from 'reactstrap';
+
+class AppNavBar extends Component {
+    // Set state for Navbar Menu
+    state = {
+        isOpen: false
+    }
+    // Set state to opposite of current state when toggled
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    render() {
+        return (
+        <div>
+            <Navbar color="dark" dark expand="sm" className="mb-5">
+                <Container>
+                    <NavbarBrand href="/">Company Name</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="/">
+                                    Dummy Link
+                                </NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Container>
+            </Navbar>
+        </div>
+        );
+    }
+}
+
+export default AppNavBar;
