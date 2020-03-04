736807042c7e9daf388060345a32ae91ed67e849
Caleb Abbott <54942782+Cabbott118@users.noreply.github.com>
2020-03-03 19:51:41 -0500

    Initial Model Created

---
+++ models/Item.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const ItemScehma = new Schema({
+    brand: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    dimensions: {
+        type: String,
+        required: true
+    },
+    weight: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = Item = mongoose.model('item', ItemScehma);
